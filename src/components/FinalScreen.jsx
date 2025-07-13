ff229eb0fa1fe00eb516102ec37f052a8f855831
MayanKiz <140157357+MayanKiz@users.noreply.github.com>
2025-07-13 21:49:49 +0530

    Update FinalScreen.jsx

--- src/components/FinalScreen.jsx
+++ src/components/FinalScreen.jsx
@@ -10,21 +10,20 @@ export default function FinalScreen({ ...motionProps }) {
 
     const finalMessage =
         `Even itne miles door hokar bhi...
-main bas aapke baare me hi sochta hoon.
-Har second jab aap se baat nahi hoti —
+Har din jab aap se baat nahi hoti —
 lagta hai kuch adhoora reh gaya.
 
 Aapki smile, woh random laugh,
-voice notes... ya woh funny Free Fire wale moments —
+wo voice notes... ya woh funny Free Fire wale moments —
 sab kuch yaad aata hai....
 
 Kabhi kabhi raat ko, bina kisi reason ke, bas apka naam sochta hoon.
 Na reply aana hota hai, na expectation hoti hai...
 fir bhi....
 
-Sochta hoon — kya usse yaad aata hoga?
-Kya kabhi usne bhi randomly meri profile dekhi hogi?
-Kya uske phone me aaj bhi koi purana moment hoga jiska ss liya hoga?
+Sochta hoon — kya apko yaad aata hoga?
+Kya kabhi apnne bhi randomly meri profile dekhi hogi?
+Kya apke phone me aaj bhi koi purana moment hoga jiska ss liya hoga?...ya koi photos...!
 
 Main apko blame nahi karta ab.
 Shayad main us waqt itna samajhne layak hi nahi tha.
@@ -38,13 +37,14 @@ kabhi kabhi raat me lagta hai:
 “Koi hoti toh memes par reply aata”
 “Koi hoti toh mujhe bhi ‘good night’ ka reason milta”
 
-Sochta hoon — woh hasti thi, toh main feel karta tha ki life thodi easy hai.
-Aur ab... jab silence hai, toh main khud se baatein karta hoon uske liye.
+Sochta hoon — woh hasti thi,
+3k wo crocodile wala moment jab apka mic open tha....ap has rhe the ....toh mughey bhut achha laga tha mtlb ki game har kar bhi jeet gya thaa .....!!
+Aur ab... 🥺!!
 
 Humne sirf chat ki thi,
 lekin connection real tha.
 Game ke beech wale wo stupid jokes,
-aur aapka “arre revive karo bhai” kehna —aaj bhi wo yaad hh jab maine ek baar revive nhi kiya tha fir jab mai eliminate hogya tha apne bola tha ki ab revive karu ya nhi vending machine ke pass khade hokar....
+aur aapka "jump nhi horra" kehna —aaj bhi wo yaad hh aur ek jab maine ek baar revive nhi kiya tha fir jab mai eliminate hogya tha apne bola tha ki ab revive karu ya nhi vending machine ke pass khade hokar....
 ab bhi sab kuch kaan me ghoomta hai......
 yaad karke sach me bhut yaad aati hh apkii 😔
 
@@ -52,17 +52,15 @@ Aap meri shanti thi
 is noisy world me —
 ek woh name jisese baat kar ke din theek lagta tha.
 
-Distance matter karta hai,
-
-but yaar kuch friends hote h bhut door door hokar bhi rehte hh ...khair ab mughey toh pata bhi nhi hh kyu aap hamse baat nhi karti...
-
 Mujhe pata nahi kab fir baat hogi,
 Ab hogi ya kabhi nhii kya ye jo sab likha hh ab tak phuchega ya nhii ....
 kab game me team banegi
 ya kab ek “hi” wapas aayega...
 but tab tak, sirf ek hi baat kehni hai —
-
-I miss you... more than my own words can ever explain. 🌙💬🎮💕`
+Thankyou for these memories and for Your precious time to read this..!!!
+ 
+I miss you... more than my own words can ever explain.
+ 🌙💬🎮💕`
 
     useEffect(() => {
         const timer = setTimeout(() => {
